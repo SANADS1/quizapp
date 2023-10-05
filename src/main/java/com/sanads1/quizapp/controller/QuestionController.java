56b9ac2677090562ef04c8e2c438631dac775a0f
Sanads1 <yarazaq99@gmail.com>
2023-10-05 06:53:07 -0700

    Implemented the Update and GetById and Service and Endpoints

--- src/main/java/com/sanads1/quizapp/controller/QuestionController.java
+++ src/main/java/com/sanads1/quizapp/controller/QuestionController.java
@@ -2,13 +2,10 @@ package com.sanads1.quizapp.controller;
 
 import com.sanads1.quizapp.model.Question;
 import com.sanads1.quizapp.service.QuestionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("question")
@@ -22,8 +19,31 @@ public class QuestionController {
 
          return _service.getAllQuestions();
      }
+     @GetMapping("{id}")
+     public Optional<Question> getQuestionById(@PathVariable("id") Integer id){
+        return _service.getQuestionById(id);
+     }
+
      @GetMapping("category/{category}")
-     public List<Question> getQuestionsByCategory(@PathVariable String category){  //We didn't pass the variable to the path like @PathVariable("category") cause the mapping route name is the same thing as the dynamic path name i.e {category}.
+     public List<Question> getQuestionsByCategory(@PathVariable String category){  //I didn't pass the variable to the path like @PathVariable("category") cause the mapping route name is the same thing as the dynamic path name i.e {category}.
           return _service.getQuestionsByCategory(category);
      }
+
+     @PostMapping("add")
+     public String addQuestion(@RequestBody Question question){
+          return _service.addQuestion(question);
+     }
+
+     @PutMapping("update/{id}")
+     public String updateQuestion(@PathVariable("id") Integer id, @RequestBody Question updatedQuestion) {
+          // Here, you should set the ID of the updatedQuestion to the provided id
+          updatedQuestion.setId(id);
+
+          return _service.updateQuestion(updatedQuestion);
+     }
+     @DeleteMapping("{id}")
+     public void deleteQuestion(@PathVariable("id") Integer id){
+        _service.deleteQuestion(id);
+     }
+
 }
--- src/main/java/com/sanads1/quizapp/service/QuestionService.java
+++ src/main/java/com/sanads1/quizapp/service/QuestionService.java
@@ -1,12 +1,15 @@
 package com.sanads1.quizapp.service;
 
+import ExceptionHandling.ResourceNotFoundException;
 import com.sanads1.quizapp.repository.QuestionRepository;
 import com.sanads1.quizapp.model.Question;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -26,4 +29,48 @@ public class QuestionService {
         logger.info("Found {} questions for category: {}", questions.size(), category);
         return questions;
     }
+
+    public String addQuestion(Question question){
+        _question.save(question);
+        return "success";
+    }
+
+    public String updateQuestion(Question updatedQuestion) {
+        // Check if the question with the given ID exists in the database
+        Optional<Question> existingQuestion = _question.findById(updatedQuestion.getId());
+
+        if (existingQuestion.isPresent()) {
+            // Perform the update
+            _question.save(updatedQuestion);
+            return "success";
+        } else {
+            // Handle the case where the question with the given ID doesn't exist
+            throw new ResourceNotFoundException("Question not found");
+        }
+    }
+
+    public Optional<Question> getQuestionById(Integer id){
+        Optional<Question> question = _question.findById(id);
+
+        if(question.isPresent()){
+            return question;
+        }else {
+            // Handle the case where the question with the given ID doesn't exist
+            throw new ResourceNotFoundException("Question with the Id cannot be found");
+        }
+
+    }
+
+    public void deleteQuestion(Integer id) {
+        Optional<Question> question = _question.findById(id);
+        if (question.isPresent()) {
+            _question.deleteById(id);
+        } else {
+            // Handle the case where the question with the given ID doesn't exist
+            throw new ResourceNotFoundException("Question with the ID " + id + " not found");
+        }
+    }
+
+
+
 }
